92174ad009f9dd6a8be0e6406dd1efebfc9d4f77
Pratham mehta <mehtapratham49@gmail.com>
2025-03-24 14:03:26 +0530

    ts-error

--- components/Landing/FrequentQuestion.tsx
+++ components/Landing/FrequentQuestion.tsx
@@ -30,7 +30,7 @@ function FrequentQuestion() {
           <AccordionItem value="item-3">
             <AccordionTrigger>Is it animated?</AccordionTrigger>
             <AccordionContent>
-              Yes. It's animated by default, but you can disable it if you
+              Yes. It &apos; s animated by default, but you can disable it if you
               prefer.
             </AccordionContent>
           </AccordionItem>
